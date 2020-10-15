d1b408308f6057ca2103aadfaf15444357f2a367
Kunzut <aniway.va@gmail.com>
2020-10-15 13:52:40 +0300

    create vue app

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <li>
+    <span v-bind:class="{done: todo.completed}">
+      <input type="checkbox" class="item-input" v-on:change="todo.completed = !todo.completed">
+      <strong class="item-id">{{index + 1}}</strong>
+      {{todo.title}}
+    </span>
+    <button class="item-btn-delete" v-on:click="$emit('remove-todo', todo.id)">
+      &times;
+    </button>
+  </li>
+</template>
+
+<script>
+
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    index: Number,
+  }
+}
+</script>
+
+<style scoped>
+  li {
+    border: 1px solid #000;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+  }
+
+  .item-id {
+    margin-right: 10px;
+  }
+
+  .item-input {
+    margin-right: 10px;
+  }
+
+  .item-btn-delete {
+    background: red;
+    color: #fff;
+    font-weight: bold;
+    border: 1px solid #000;
+    border-radius: 50%;
+    outline: none;
+  }
+
+  .done {
+    text-decoration: line-through;
+  }
+</style>
